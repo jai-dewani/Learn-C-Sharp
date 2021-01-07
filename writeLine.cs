fc09d3055ea384a2be0310bf4e6f2b1bd09c8522
jai-dewani <jai.dewani.99@gmail.com>
2021-01-07 20:30:39 +0530

    Learning...

---
+++ writeLine.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace writeLine
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a statment");
+            Console.Write("New line. ");
+            Console.Write("STILL SAME LINE");
+            Console.WriteLine("NEW LINE");
+        }
+    }
+}
