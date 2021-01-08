cc2b8fd36a49d4c42f6d27775b8be2791eef4c89
jai-dewani <jai.dewani.99@gmail.com>
2021-01-08 18:01:46 +0530

    Add a Goose Console App

---
+++ VS/Goose/Goose/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Goose
+{
+    class Goose
+    {
+        public string name;
+        public string honk;
+        public int power;
+
+        public Goose(string namePass, string honkPass, int powerPass)
+        {
+            name = namePass;
+            honk = honkPass;
+            power = powerPass;
+        }
+
+        public void HONK()
+        {
+            Console.WriteLine(name + " is ready to honk");
+            for (int i = 0; i < power; i++)
+            {
+                Console.WriteLine(honk);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Goose one = new Goose("Jai", "HONK!", 5);
+            one.HONK();
+        }
+    }
+}
